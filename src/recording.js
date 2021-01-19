8c14909dbed7be7e0470da4a46045eb85088d381
medmunds <medmunds@gmail.com>
2021-01-19 12:32:35 -0800

    Fix failure to render on current RTD releases
    ReadTheDocs has switched Google Analytics scripts.
    (Used to use `ga` function, now use `gtag`.) Update
    to work with either.
    
    ReadTheDocs now initializes GA in async script,
    which may load after RateTheDocs. Don't block widget
    if GA not found, because it might still be loading.
    (Only block if doNotTrack enabled.)

--- src/recording.js
+++ src/recording.js
@@ -4,29 +4,53 @@ const READTHEDOCS_DATA = window.READTHEDOCS_DATA || {};
 
 
 function recordEvent(action, label, value) {
-  // ReadTheDocs initializes the Google Analytics trackerName "user"
-  // with the project owner's GA trackingId
-  const ga = window.ga || console.log; // if GA not loaded, just log (for local debugging)
-  ga("user.send", "event", "RateTheDocs", action, label, value, {
-    // (dimension order matches readthedocs-analytics.js, merely to avoid confusion)
-    'dimension1': READTHEDOCS_DATA.project,
-    'dimension2': READTHEDOCS_DATA.version
-  });
+  // global gtag;
+  // global ga;
+  const isLocalhost = window.location.hostname === "localhost";
+  const category = "RateTheDocs";
+  if (typeof gtag === "function") {
+    // ReadTheDocs initializes READTHEDOCS_DATA.user_analytics_code
+    // with the project owner's GA trackingId
+    gtag("event", action, {
+      'send_to': READTHEDOCS_DATA.user_analytics_code,
+      'event_category': category,
+      'event_label': label,
+      'event_value': value,
+      'dimension1': READTHEDOCS_DATA.project,
+      'dimension2': READTHEDOCS_DATA.version
+    });
+  } else if (typeof ga === "function") {
+    // ReadTheDocs initializes the Google Analytics trackerName "user"
+    // with the project owner's GA trackingId
+    ga("user.send", "event", category, action, label, value, {
+      // (dimension order matches readthedocs-analytics.js, merely to avoid confusion)
+      'dimension1': READTHEDOCS_DATA.project,
+      'dimension2': READTHEDOCS_DATA.version
+    });
+  } else if (isLocalhost) {
+    // Local debugging without GA
+    console.log("recordEvent", category, action, label, value);
+  } else {
+    // Either attempt to submit feedback before GA loads (unlikely),
+    // or user is blocking GA but has not enabled doNotTrack.
+    alert("Unable to register your docs feedback: Google Analytics not loaded");
+  }
 }
 
 
 export default {
   init() {
-    // Return true if recording is available and can be used
-    const googleAnalyticsLoaded = Boolean(window.ga);
-    const isLocalhost = window.location.hostname === "localhost"; // local debugging w/o GA
+    // Return true if recording is available and can be used.
+    // (Google Analytics loads async, and the ReadTheDocs code that kicks
+    // it off is also loaded async—readthedocs-analytics.js—so just assume
+    // it will become available later unless the user has enabled doNotTrack.)
     const navigator = window.navigator || {};
     const doNotTrack = (
       navigator.doNotTrack === "1" // Firefox, Chrome
       || navigator.msDoNotTrack === "1" // IE 9, 10
       || navigator.doNotTrack === "yes" // Gecko < 32
       || window.doNotTrack === "1"); // Safari, IE 11, Edge
-    return !doNotTrack && (googleAnalyticsLoaded || isLocalhost);
+    return !doNotTrack;
   },
 
   recordUpVote() {
